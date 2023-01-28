7bf14c9f81521b51e3a4d26bc59bf151896a2d79
Long Nguy3n <longgnguyenn9300@gmail.com>
2023-01-28 23:14:19 +0700

    Add Animation to Player

--- Assets/Scipts/PlayerController.cs
+++ Assets/Scipts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private Animator playerAnim;
     [SerializeField] private float jumpForce = 10;
     [SerializeField] private float gravityModifier;
     [SerializeField] private bool isGrounded = true;
@@ -14,16 +15,18 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded && !gameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isGrounded= false;
+            playerAnim.SetTrigger("Jump_trig");
         }
         //GameOver();
     }
@@ -38,6 +41,8 @@ public class PlayerController : MonoBehaviour
         {
             Debug.Log("Game Over!");
             gameOver = true;
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
         }
     }
 
